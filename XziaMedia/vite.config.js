042972ed552db9d9f7fc16b05ee246a9b58addde
brijeshsonraj <brijesh.sonraj@gmail.com>
2025-05-11 17:10:22 +0530

    update vite.

--- XziaMedia/vite.config.js
+++ XziaMedia/vite.config.js
@@ -5,4 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: '0.0.0.0', // Bind to all interfaces
+    port: 5173,      // Optional: Specify the port
+    allowedHosts: ['xziamedia.com'], // Add your domain here
+  }
 })
